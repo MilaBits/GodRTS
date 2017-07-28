b5d0161b0260e7c4f645da61fd143af419fbf9ad
Liam Kisters <LiamAHK@live.nl>
2017-07-28 02:11:45 +0200

    Added new scripts and a few more models

---
+++ GodRTS/Assets/Scripts/Health.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour {
+
+    int healthPoints = 100;
+    int defencePoints = 0;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (defencePoints > 100)
+        {
+            defencePoints = 100;
+        }
+
+        if (defencePoints <= 0)
+        {
+            defencePoints = 0;
+            Debug.Log("Object defences have been decimated.");
+        }
+
+        if (healthPoints > 100)
+        {
+            healthPoints = 100;
+        }
+
+        if(healthPoints <= 0)
+        {
+            healthPoints = 0;
+            Debug.Log("Object has been decimated.");
+        }
+        
+	}
+}
